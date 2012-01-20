f78babef3351dfcff3ef6183838096e08478fd84
Phillip Gomez <gomez.phillip@gmail.com>
2012-01-20 11:18:09 -0800

    Adding Peepcode/BenHoskings git prompt scripts. Modified to show new untracked files

---
+++ bin/git-cwd-info.rb
@@ -0,0 +1,49 @@
+#!/usr/bin/env ruby
+# -*- coding: utf-8 -*-
+
+# Emits Git metadata for use in a Zsh prompt.
+#
+# AUTHOR:
+#    Ben Hoskings
+#   https://github.com/benhoskings/dot-files/blob/master/files/bin/git_cwd_info
+#
+# MODIFIED:
+#    Geoffrey Grosenbach http://peepcode.com
+
+# The methods that get called more than once are memoized.
+
+def git_repo_path
+  @git_repo_path ||= `git rev-parse --git-dir 2>/dev/null`.strip
+end
+
+def in_git_repo
+  !git_repo_path.empty? &&
+  git_repo_path != '~' &&
+  git_repo_path != "#{ENV['HOME']}/.git"
+end
+
+def git_parse_branch
+  @git_parse_branch ||= `git-current-branch`.chomp
+end
+
+def git_head_commit_id
+  `git rev-parse --short HEAD 2>/dev/null`.strip
+end
+
+def git_cwd_dirty
+  " ✗" unless git_repo_path == '.' || `git ls-files -m -o --exclude-standard`.strip.empty?
+end
+
+def rebasing_etc
+  if File.exists?(File.join(git_repo_path, 'BISECT_LOG'))
+    "+bisect"
+  elsif File.exists?(File.join(git_repo_path, 'MERGE_HEAD'))
+    "+merge"
+  elsif %w[rebase rebase-apply rebase-merge ../.dotest].any? {|d| File.exists?(File.join(git_repo_path, d)) }
+    "+rebase"
+  end
+end
+
+if in_git_repo
+  print " %F{5}#{git_parse_branch}%f@%F{4}#{git_head_commit_id}%f#{rebasing_etc}%F{9}#{git_cwd_dirty}%f"
+end
